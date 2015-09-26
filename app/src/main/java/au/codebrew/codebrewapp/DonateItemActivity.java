e695fbe51ff49b26e8c3fca3d9e45d93481470a8
sym44 <raymond.seki@gmail.com>
2015-09-26 23:57:49 +1000

    # Add List Views

--- app/src/main/java/au/codebrew/codebrewapp/DonateItemActivity.java
+++ app/src/main/java/au/codebrew/codebrewapp/DonateItemActivity.java
@@ -1,17 +1,24 @@
 package au.codebrew.codebrewapp;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class DonateItemActivity extends AppCompatActivity {
     private ImageView imageView = null;
     private Spinner spinner_country = null;
     private Spinner spinner_state = null;
+    private ListView listView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,46 @@ public class DonateItemActivity extends AppCompatActivity {
                 R.array.planets_array, R.layout.support_simple_spinner_dropdown_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_state.setAdapter(adapter2);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> causesList = new ArrayList<String>();
+        causesList.add("155 Pelham Street, Carlton");
+        causesList.add("23 - 47 Villiers St, North Melbourne");
+        causesList.add("2/360 Bourke Street, Melbourne");
+        causesList.add("100-154 Batman St, West Melbourne");
+        causesList.add("191 Bridge Rd, Richmond");
+
+        final ArrayList<String> descriptionList = new ArrayList<String>();
+        descriptionList.add("1300 367 428");
+        descriptionList.add("(03) 9345 1800");
+        descriptionList.add("(03) 9421 6481");
+        descriptionList.add("(03) 9427 8556");
+        descriptionList.add("13 14 95");
+
+        final MyListAdapter adapter = new MyListAdapter(this, causesList, descriptionList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @SuppressLint("NewApi")
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+//                                causesList.remove(item);
+//                                descriptionList.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
     }
 
     @Override
