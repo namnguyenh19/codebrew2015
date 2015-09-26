1dc6cb83ce09471e800aea74a9a9b6b5769403e2
sym44 <raymond.seki@gmail.com>
2015-09-26 20:04:07 +1000

    # New Introduction Page

--- app/src/main/java/au/codebrew/codebrewapp/MainActivity.java
+++ app/src/main/java/au/codebrew/codebrewapp/MainActivity.java
@@ -1,5 +1,7 @@
 package au.codebrew.codebrewapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +12,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // pass to the intro activity for the first time the app is opened
+        // return to the main activity
+        SharedPreferences prefs = getSharedPreferences("prefs", 0);
+        boolean firstRun = prefs.getBoolean("firstRun", false);
+        if(firstRun == false) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstRun",true);
+            editor.commit();
+            Intent intent = new Intent(MainActivity.this,IntroActivity.class);//Activity to be     launched For the First time
+            startActivity(intent);
+            finish();
+        }
+//        else {
+//            Intent intent = new Intent(MainActivity.this, MainActivity.class);//Default Activity
+//            startActivity(intent);
+//            finish();
+//        }
+
         setContentView(R.layout.activity_main);
     }
 
